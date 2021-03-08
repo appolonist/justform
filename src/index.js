40d546f036624dab0c0f4addbf0217cfbb056a5e
Karol <appolonist@gmail.com>
2021-03-08 17:57:03 +0000

    Simple config and component implemented

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => (
+    <div>
+        <h1>
+            Text from form!
+        </h1>
+    </div>
+);
+
+ReactDOM.render(<App/>, document.getElementById("root"));
